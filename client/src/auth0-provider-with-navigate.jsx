bd3c5acde1d012b02d034e1549dd2dcee6af8340
Olena Boldysheva <adv55577@gmail.com>
2023-03-16 02:24:55 +0200

    Update auth0-provider-with-navigate.jsx

--- client/src/auth0-provider-with-navigate.jsx
+++ client/src/auth0-provider-with-navigate.jsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
@@ -7,8 +8,12 @@ export const Auth0ProviderWithNavigate = ({ children }) => {
 
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
   const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+  // const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+  const redirectUri =
+    import.meta.env.VITE_VERCEL_ENV === "production"
+      ? `https://${window.location.hostname}/callback`
+      : import.meta.env.VITE_AUTH0_CALLBACK_URL;
 
   const onRedirectCallback = (appState) => {
     navigate(appState?.returnTo || window.location.pathname);
